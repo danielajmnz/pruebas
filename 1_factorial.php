8e558b7b65215256012547456f16ad7c85962153
danielajmnz <49690652+danielajmnz@users.noreply.github.com>
2019-04-16 11:47:15 -0500

    factorial
    Es un ejemplo de factorial con errores

---
+++ 1_factorial.php
@@ -0,0 +1,35 @@
+<?php
+function factorial($numero)
+{
+    if($numero >= 2){
+        return ($numero * factorial($numero - 1));
+    }else{
+        return true;
+    }
+}
+if($_POST["enviar"] == "Calcular")
+{
+	 
+	$numero = $_POST["numero"];
+	
+	echo 'el factorial de $numero es '.factorial($numero);
+}
+?>
+<html>
+	<head>
+		<title>
+		Ejercicio factorial
+		</title>
+	</head>
+	<body>
+	
+		<form id="frmFactorial" name="frmFactorial" action="prueba_1.php" method="get" >
+		
+			Ingrese el numero del que quiere obtener factorial: <input type="text" name="numero" id="numero" value="" >
+			<br><br>
+			<input type="button" name="enviar" id="enviar" value="Calcular">
+		
+		</form>
+		
+	</body>
+</html>
